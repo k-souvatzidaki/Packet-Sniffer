40189d82a66f3f918473fde94df5c93d22c6d91f
k-souvatzidaki <kwn.souv@gmail.com>
2021-02-28 21:46:08 +0200

    Added IPv4

--- sniffer.py
+++ sniffer.py
@@ -3,12 +3,25 @@ import pcap, unpacking
 def main():
     cap = pcap.pcap(None)
     print('listening on %s: %s' % (cap.name, cap.filter))
+    print("--------------------------------------------------------------------------------------")
     
+    #Support for IPv4 packets
+    i = 1
     for d, packet in cap:
         #unpacking MAC (Ethernet) header
-        dst, src, mac_data = unpacking.unpack_MAC(packet)
+        dst, src, ethertype, mac_data = unpacking.unpack_MAC(packet)
+        #print packet length in bytes
+        print("Packet #"+str(i) +" Length = "+str(len(packet)))
         print "Destination MAC: " + dst + ", Source MAC: " + src
-        #TODO unpacking IP header
-        #unpacking.unpack_IP(mac_data)
+
+
+        if(ethertype == int('0800',16)): #IPv4
+            print("Network Layer protocol: IPv4")
+            #unpacking IP header
+            dst, src = unpacking.unpack_IPv4(mac_data)
+            print "Destination IP: " + str(dst) + ", Source IP: " + str(src)
+        
+        i = i+1
+        print("--------------------------------------------------------------------------------------")
 
 main()
--- unpacking.py
+++ unpacking.py
@@ -1,20 +1,56 @@
 import struct, binascii
 
+# MAC address formatting
 def formatting_MAC(i):
     return '-'.join(a+b for a,b in zip(i[::2], i[1::2]))
 
+# IPv4 address formatting
+def formatting_IPv4(i):
+    #TODO
+    return i
+
+# convert bytes to hexadecimal 
 def hex(i):
     return binascii.hexlify(i).upper()
 
+
+# LAYER 2 : DATA LINK LAYER - MAC 
+# Header format: 
+#  _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
+# |   MAC dst(48 bits)  |  MAC src(48 bits)  | Ethertype(16 bits) |
+
 def unpack_MAC(data):
-    dst, src , proto = struct.unpack('!6s6s2s',data[:14]) 
+    #unpack the MAC header
+    dst, src , ethertype = struct.unpack('!6s6s2s',data[:14]) 
     dst_hex = hex(dst)
     src_hex = hex(src)
-    #return destination and source in hexadecimal, and the rest of the packet
-    return formatting_MAC(dst_hex), formatting_MAC(src_hex), data[14:]
+    #return destination and source in hexadecimal, ethertype as an intiger and the MAC payload
+    return formatting_MAC(dst_hex), formatting_MAC(src_hex), int(hex(ethertype),16), data[14:]
 
-def unpack_IP(data):
+
+# LAYER 3: NETWORK
+
+# IPv4
+# Header format: 
+#  _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
+# |       Version|IHL|DSCP|ECN|Total Length (32 bits total)       |
+#  _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
+# |           ID|Flags|Fragment offset (32 bits total)            |
+#  _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
+# |                       Src IP (32 bits)                        |
+#  _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
+# |                       Dst IP (32 bits)                        |
+#  _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
+# |                 Options if IHL > 5 (32 bits)                  |
+
+def unpack_IPv4(data):
+    #unpack the IPv4 header
     src, dst= struct.unpack('!12x4s4s',data[:20]) 
-    #TODO format IP addresses
+    formatted_src = formatting_IPv4(struct.unpack('{}B'.format(len(src)),src))
+    formatted_dst = formatting_IPv4(struct.unpack('{}B'.format(len(dst)),dst))
+    #return formatted destination and source
+    return formatted_src,formatted_dst
 
-#TODO unpack transport layer
+
+# LAYER 4: TRANSPORT 
+#TODO
